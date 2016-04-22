e24fe820c3bd4fa348636ba267089bd5c5b74d04
Andrew Thorp <andrewpthorp@gmail.com>
2016-04-21 21:35:27 -0500

    add Unchained.new

--- lib/unchained.rb
+++ lib/unchained.rb
@@ -4,5 +4,10 @@ require_relative 'unchained/error'
 
 module Unchained
   extend Configuration
+
+  # Delegate to Unchained::Client.new
+  def self.new(opts={})
+    Unchained::Client.new(opts)
+  end
 end
 
