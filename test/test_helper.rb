1600cdb0f1fefefc89df3c8f96b8b96b52d730cd
Andrew Thorp <andrewpthorp@gmail.com>
2016-04-24 16:57:59 -0500

    use minitest-reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'unchained'
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = "test/_fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
+
+
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 
--- unchained.gemspec
+++ unchained.gemspec
@@ -29,5 +29,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'vcr', '~> 3.0'
   s.add_development_dependency 'minitest', '~> 5.8'
   s.add_development_dependency 'pry', '~> 0.10'
+  s.add_development_dependency 'minitest-reporters', '~> 1.1'
   s.add_dependency 'rest-client', '~> 1.8'
 end
